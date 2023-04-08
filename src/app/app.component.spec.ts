c8e66cdc527159e13b68718fe380a153c4bdc99a
Md.Al-amin <al.amin@tigerit.com>
2023-04-08 19:15:36 +0600

    AutoCompleteTrieSearch Test Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AutoCompleteTrieSearch, INodeValue } from 'autocomplete-trie-search';
+import * as uuid from 'uuid';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -14,22 +16,94 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Performance test by inserting 10000 data', () => {
+    const autoCompleteTrieSearch = new AutoCompleteTrieSearch();
+    const start = performance.now();
+
+    for(let i = 0; i<10000; i++){
+      const node: INodeValue = {
+        text: uuid.v4(),
+        value: {id: uuid.v4(), text: i},
+        weight: 1
+      };
+      autoCompleteTrieSearch.insertOrUpddate(node);
+    }
+    
+    const takenTime = performance.now() - start;
+
+    console.log("Time taken to insert 100000 data: " + takenTime + "milisecond.");
+    expect(takenTime).toBeLessThan(1000);
+
   });
 
-  it(`should have as title 'autocomplete_trie_search_test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('autocomplete_trie_search_test');
+  it('Suggestion performances by inserting are 10000 data', () => {
+    const search = new AutoCompleteTrieSearch({maxSuggestion: 10, allowedMismatchCount: 3});
+
+    for(let i = 0; i<10000; i++){
+      const node: INodeValue = {
+        text: uuid.v4(),
+        value: {id: uuid.v4(), text: i},
+        weight: 1
+      };
+      search.insertOrUpddate(node);
+    }
+
+    var list = [
+      {
+        text: "hello123world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 4
+      },
+      {
+        text: "hello231world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 5
+      },
+      {
+        text: "hello3423world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 6
+      },
+      {
+        text: "hello423world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 7
+      },
+      {
+        text: "hello53world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 8
+      },
+      {
+        text: "hello43543world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 9
+      },
+      {
+        text: "hello4534world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 11
+      },
+      {
+        text: "hello545world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 12
+      },
+      {
+        text: "hello4543world",
+        value: {id: uuid.v4(), text: 'AL AMIN'},
+        weight: 13
+      }
+    ]
+    
+    search.insertOrUpddate(list);
+    
+    const sStart = performance.now();
+
+    console.log('Autocomplete suggessions1 => ', search.suggession("hello"));
+    const takenForSuggesion = performance.now() - sStart;
+    console.log("Time taken for providing suggesion : "+ takenForSuggesion + " in millisecond.", 'nodecount: ', search.nodeCount)
+    expect(takenForSuggesion).toBeLessThan(1000)
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('autocomplete_trie_search_test app is running!');
-  });
 });
